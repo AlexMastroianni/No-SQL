832054e7ad851741bcebb957c9661f0caaa0c148
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-12-05 20:59:53 +1100

    updating userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,10 +10,8 @@ const {
   removeFriend,
 } = require('../../controllers/userContoller');
 
-// Set up GET all and POST at /api/users
 router.route('/').get(getAllUser).post(createUser);
 
-// Set up GET one, PUT, and DELETE at /api/users/:id
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
